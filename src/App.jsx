7b26f5018e4ee03b27823999f5912d8ca7926b66
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-14 11:58:15 +0530

    basic setup and structure done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/cart/Cart";
+import Shop from "./pages/shop/Shop";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
